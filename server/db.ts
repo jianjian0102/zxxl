05714525d3c12caf317cb709962fd9ee6fe2597a
jianjian0102 <jianhongyu98@gmail.com>
2025-12-22 22:34:21 +0900

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -5,10 +5,15 @@ import * as schema from "@shared/schema";
 const { Pool } = pg;
 
 if (!process.env.DATABASE_URL) {
-  throw new Error(
-    "DATABASE_URL must be set. Did you forget to provision a database?",
-  );
+  throw new Error("DATABASE_URL must be set.");
 }
 
-export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  // 强制开启 SSL 并允许自签名证书
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 export const db = drizzle(pool, { schema });
